a40243b98464e0c41af6f45924bfaeb39e7360f8
MarcosRZ <marcos.rodriguez@clarcat.com>
2018-09-17 16:09:01 +0200

    Added colors, and other minor stuff

---
+++ src/config/app.config.js
@@ -0,0 +1,9 @@
+const APP_NAME = 'DPL-API';
+const VERSION = '0.1';
+const DEFAULT_PORT = 8080;
+
+export default {
+  APP_NAME,
+  VERSION,
+  DEFAULT_PORT
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var app = express();
-var port = process.argv[2] || 8080;
+import express from 'express';
+import colors from 'colors';
+import CONFIG from './config/app.config';
+
+const app = express();
+const port = process.argv[2] || CONFIG.DEFAULT_PORT;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(colors.green(`[${colors.magenta(CONFIG.APP_NAME)}] up and running at port [${colors.yellow(port)}]!`));
 });
